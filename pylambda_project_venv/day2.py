c115a6c1dd6ddadbbfacdcecda8585189e795f73
aibidapo <24958100+aibidapo@users.noreply.github.com>
2023-01-29 12:48:10 -0600

    first commit

---
+++ pylambda_project_venv/day2.py
@@ -0,0 +1,47 @@
+#
+# user_prompt = input("Enter a 'todo: ")
+#
+# to_dos = []
+#
+# response = user_prompt
+#
+# print(response)
+#
+# while response != 'done':
+#
+#     print(user_prompt)
+#
+#     # print("Enter a todo: ")
+
+    # print(user_prompt)
+
+
+# user_prompt = input("Enter a 'todo: ")
+
+response = True
+
+tasks = []
+
+while response != 'done':
+
+    response = input("Enter a 'todo: ")
+    # print(response)
+    print("Next item....")
+
+    tasks.append(response)
+
+print(tasks)
+
+
+    #
+    # if task == 'done':
+    #     break
+
+
+
+
+
+
+
+
+
