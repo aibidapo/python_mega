2d26c4fb0855a780386ff6a6d986ec24348a360b
aibidapo <24958100+aibidapo@users.noreply.github.com>
2023-01-31 02:43:04 -0600

    day 3 exercises

---
+++ day3_exercise1.py
@@ -0,0 +1,27 @@
+# Create a program that does the following:
+#
+# 1. Prompts the user to input the country they are from.
+#
+# 2. If the user enters the word USA, the program prints out Hello.
+#
+# 3. If the user enters the word  India, the program prints out Namaste.
+#
+# 4. If the user enters the word Germany, the program prints out Hallo.
+#
+# Note: Strings are case-sensitive in Python, meaning germany and Germany are treated as two different strings.
+
+
+country = input("What country are you from? ")
+
+country = country.lower()
+
+match country:
+    case 'usa':
+        print("Hello")
+    case 'india':
+        print('Namaste')
+    case 'germany':
+        print("Hallo")
+
+    case unknown:
+        print(f"How do you say 'hello' in {unknown.upper()}")
